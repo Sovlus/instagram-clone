6df67667f151e4979f4cc381fad4c50de8ae6c08
Sovlus <kubbaz122@gmail.com>
2023-05-24 18:39:50 +0200

    added divs to homepage

--- src/Homepage.js
+++ src/Homepage.js
@@ -2,7 +2,13 @@ import React from "react";
 import Homepage from "./Homepage.css";
 
 function Homepage() {
-  return <div className='wrapper'></div>;
+  return (
+    <div className='wrapper'>
+      <div className='nav'>
+        <div className='timeline'></div>
+      </div>
+    </div>
+  );
 }
 
 export default Homepage;
