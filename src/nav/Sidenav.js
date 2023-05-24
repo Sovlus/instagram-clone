a6778100b9154b2f27cce17f8c30e5aa130f4555
Sovlus <kubbaz122@gmail.com>
2023-05-24 18:59:45 +0200

    added image and sidenav files

--- src/nav/Sidenav.js
+++ src/nav/Sidenav.js
@@ -2,7 +2,11 @@ import React from "react";
 import "./Sidenav.css";
 
 function Sidenav() {
-  return <div>Sidenav</div>;
+  return (
+    <div className='wrapper'>
+      <img src='https://i.imgur.com/dz5U2V3.png' alt='instagram logo' />
+    </div>
+  );
 }
 
 export default Sidenav;
