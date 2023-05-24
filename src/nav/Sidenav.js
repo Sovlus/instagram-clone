c350a4e14a65191d2361bb88f5e998fb9bf1a3f5
Sovlus <kubbaz122@gmail.com>
2023-05-24 19:05:42 +0200

    added instagram logo and kafelki

--- src/nav/Sidenav.js
+++ src/nav/Sidenav.js
@@ -3,8 +3,42 @@ import "./Sidenav.css";
 
 function Sidenav() {
   return (
-    <div className='wrapper'>
-      <img src='https://i.imgur.com/dz5U2V3.png' alt='instagram logo' />
+    <div className='sidenav'>
+      <img
+        className='sidenav__logo'
+        src='https://www.pngkey.com/png/full/828-8286178_mackeys-work-needs-no-elaborate-presentation-or-distracting.png'
+        alt='Instagram Logo'
+      />
+
+      <div className='kafelki'>
+        <button classname='kafelek'>
+          <span>Home</span>
+        </button>
+
+        <button classname='kafelek'>
+          <span>Home</span>
+        </button>
+
+        <button classname='kafelek'>
+          <span>Home</span>
+        </button>
+
+        <button classname='kafelek'>
+          <span>Home</span>
+        </button>
+
+        <button classname='kafelek'>
+          <span>Home</span>
+        </button>
+
+        <button classname='kafelek'>
+          <span>Home</span>
+        </button>
+
+        <button classname='kafelek'>
+          <span>Home</span>
+        </button>
+      </div>
     </div>
   );
 }
